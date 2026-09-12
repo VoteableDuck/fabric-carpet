396cd1c73cbfa6431aa7e728b290cc4e77c2dd1b
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:35:35 +0800

    Compose Scarpet structure postprocess hook

--- src/main/java/carpet/mixins/StructurePiece_scarpetPlopMixin.java
+++ src/main/java/carpet/mixins/StructurePiece_scarpetPlopMixin.java
@@ -1,22 +1,25 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.chunk.ChunkAccess;
 import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(StructurePiece.class)
 public class StructurePiece_scarpetPlopMixin
 {
-    @Redirect(method = "placeBlock", at = @At(
+    @WrapOperation(method = "placeBlock", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/chunk/ChunkAccess;markPosForPostprocessing(Lnet/minecraft/core/BlockPos;)V"
     ))
-    private void markOrNot(ChunkAccess chunk, BlockPos pos)
+    private void markOrNot(ChunkAccess chunk, BlockPos pos, Operation<Void> original)
     {
-        if (!CarpetSettings.skipGenerationChecks.get()) chunk.markPosForPostprocessing(pos);
+        if (!CarpetSettings.skipGenerationChecks.get()) {
+            original.call(chunk, pos);
+        }
     }
 }
