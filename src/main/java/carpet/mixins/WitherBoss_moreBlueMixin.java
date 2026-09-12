1d9665f7eab97d1ab49bbced467f2ec417c13cc0
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:28:27 +0800

    Compose blue wither skull random hook

--- src/main/java/carpet/mixins/WitherBoss_moreBlueMixin.java
+++ src/main/java/carpet/mixins/WitherBoss_moreBlueMixin.java
@@ -1,25 +1,24 @@
 package carpet.mixins;
 
-
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import net.minecraft.world.entity.boss.wither.WitherBoss;
 
 @Mixin(WitherBoss.class)
 public class WitherBoss_moreBlueMixin
 {
-    @Redirect(method = "performRangedAttack(ILnet/minecraft/world/entity/LivingEntity;)V", at = @At(
+    @WrapOperation(method = "performRangedAttack(ILnet/minecraft/world/entity/LivingEntity;)V", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/util/RandomSource;nextFloat()F")
     )
-    private float nextFloatAmplfied(RandomSource random)
+    private float nextFloatAmplfied(RandomSource random, Operation<Float> original)
     {
-        if (CarpetSettings.moreBlueSkulls) return random.nextFloat()/100;
-        return random.nextFloat();
+        float value = original.call(random);
+        if (CarpetSettings.moreBlueSkulls) return value / 100.0F;
+        return value;
     }
-
 }
