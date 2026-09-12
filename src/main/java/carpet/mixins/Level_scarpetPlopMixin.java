d9ed1bdd65fdd38b5605f68ce4d972da4863c24d
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:29:39 +0800

    Compose Scarpet heightmap sampling hook

--- src/main/java/carpet/mixins/Level_scarpetPlopMixin.java
+++ src/main/java/carpet/mixins/Level_scarpetPlopMixin.java
@@ -1,31 +1,30 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraft.world.level.levelgen.Heightmap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Level.class)
 public class Level_scarpetPlopMixin
 {
 
-    @Redirect(method = "getHeight", at = @At(
+    @WrapOperation(method = "getHeight", at = @At(
             value = "INVOKE",
             target = "net/minecraft/world/level/chunk/LevelChunk.getHeight(Lnet/minecraft/world/level/levelgen/Heightmap$Types;II)I"
     ))
-    private int fixSampleHeightmap(LevelChunk chunk, Heightmap.Types type, int x, int z)
+    private int fixSampleHeightmap(LevelChunk chunk, Heightmap.Types type, int x, int z, Operation<Integer> original)
     {
+        Heightmap.Types sampledType = type;
         if (CarpetSettings.skipGenerationChecks.get())
         {
-            Heightmap.Types newType = type;
-            if (type == Heightmap.Types.OCEAN_FLOOR_WG) newType = Heightmap.Types.OCEAN_FLOOR;
-            else if (type == Heightmap.Types.WORLD_SURFACE_WG) newType = Heightmap.Types.WORLD_SURFACE;
-            return chunk.getHeight(newType, x, z);
+            if (type == Heightmap.Types.OCEAN_FLOOR_WG) sampledType = Heightmap.Types.OCEAN_FLOOR;
+            else if (type == Heightmap.Types.WORLD_SURFACE_WG) sampledType = Heightmap.Types.WORLD_SURFACE;
         }
-        return chunk.getHeight(type, x, z);
+        return original.call(chunk, sampledType, x, z);
     }
 }
-
