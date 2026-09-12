681d1c27d27533e06f6415cac71f2fca51428e30
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:27:22 +0800

    Compose temple husk spawn visibility hook

--- src/main/java/carpet/mixins/Monster_templesMixin.java
+++ src/main/java/carpet/mixins/Monster_templesMixin.java
@@ -2,6 +2,8 @@ package carpet.mixins;
 
 import carpet.CarpetSettings;
 import carpet.utils.SpawnOverrides;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
@@ -13,16 +15,15 @@ import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Monster.class)
 public class Monster_templesMixin
 {
-    @Redirect(method = "checkSurfaceMonstersSpawnRules", at = @At(value = "INVOKE", target="Lnet/minecraft/world/level/ServerLevelAccessor;canSeeSky(Lnet/minecraft/core/BlockPos;)Z"))
-    private static boolean isSkylightOrTempleVisible(ServerLevelAccessor serverWorldAccess, BlockPos pos,
+    @WrapOperation(method = "checkSurfaceMonstersSpawnRules", at = @At(value = "INVOKE", target="Lnet/minecraft/world/level/ServerLevelAccessor;canSeeSky(Lnet/minecraft/core/BlockPos;)Z"))
+    private static boolean isSkylightOrTempleVisible(ServerLevelAccessor serverWorldAccess, BlockPos pos, Operation<Boolean> original,
                                                      EntityType<? extends Mob> entityType, ServerLevelAccessor serverLevelAccessor, EntitySpawnReason entitySpawnReason, BlockPos blockPos, RandomSource randomSource)
     {
-        return serverWorldAccess.canSeeSky(pos) ||
+        return original.call(serverWorldAccess, pos) ||
                 (CarpetSettings.huskSpawningInTemples && entityType == EntityType.HUSK && SpawnOverrides.isStructureAtPosition((ServerLevel)serverWorldAccess, BuiltinStructures.DESERT_PYRAMID, pos));
     }
 }
