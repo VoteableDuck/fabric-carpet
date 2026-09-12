3dbb8f4bdba96df698f00bb98e8f49cc0e5e668e
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:28:20 +0800

    Compose placement direction rotation hooks

--- src/main/java/carpet/mixins/DirectionMixin.java
+++ src/main/java/carpet/mixins/DirectionMixin.java
@@ -3,26 +3,27 @@ package carpet.mixins;
 import carpet.CarpetSettings;
 import carpet.fakes.EntityInterface;
 import carpet.helpers.BlockRotator;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Direction.class)
 public abstract class DirectionMixin
 {
-    @Redirect(method = "orderedByNearest", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewYRot(F)F"))
-    private static float getYaw(Entity entity, float float_1)
+    @WrapOperation(method = "orderedByNearest", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewYRot(F)F"))
+    private static float getYaw(Entity entity, float partialTick, Operation<Float> original)
     {
         float yaw;
         if (!CarpetSettings.placementRotationFix)
         {
-            yaw = entity.getViewYRot(float_1);
+            yaw = original.call(entity, partialTick);
         }
         else
         {
-            yaw = ((EntityInterface) entity).getMainYaw(float_1);
+            yaw = ((EntityInterface) entity).getMainYaw(partialTick);
         }
         if (BlockRotator.flippinEligibility(entity))
         {
@@ -30,10 +31,11 @@ public abstract class DirectionMixin
         }
         return yaw;
     }
-    @Redirect(method = "orderedByNearest", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewXRot(F)F"))
-    private static float getPitch(Entity entity, float float_1)
+
+    @WrapOperation(method = "orderedByNearest", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;getViewXRot(F)F"))
+    private static float getPitch(Entity entity, float partialTick, Operation<Float> original)
     {
-        float pitch = entity.getViewXRot(float_1);
+        float pitch = original.call(entity, partialTick);
         if (BlockRotator.flippinEligibility(entity))
         {
             pitch = -pitch;
