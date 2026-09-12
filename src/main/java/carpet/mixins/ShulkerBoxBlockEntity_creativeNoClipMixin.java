46f7044761b36efbf3712194d3f571a4e6311f07
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:25:48 +0800

    Compose creative no-clip shulker collision hook

--- src/main/java/carpet/mixins/ShulkerBoxBlockEntity_creativeNoClipMixin.java
+++ src/main/java/carpet/mixins/ShulkerBoxBlockEntity_creativeNoClipMixin.java
@@ -1,25 +1,26 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.world.level.material.PushReaction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ShulkerBoxBlockEntity.class)
 public class ShulkerBoxBlockEntity_creativeNoClipMixin
 {
-    @Redirect(method = "moveCollidedEntities", at = @At(
+    @WrapOperation(method = "moveCollidedEntities", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/Entity;getPistonPushReaction()Lnet/minecraft/world/level/material/PushReaction;"
     ))
-    private PushReaction getPistonBehaviourOfNoClipPlayers(Entity entity)
+    private PushReaction getPistonBehaviourOfNoClipPlayers(Entity entity, Operation<PushReaction> original)
     {
-        if (CarpetSettings.creativeNoClip && entity instanceof Player && (((Player) entity).isCreative()) && ((Player) entity).getAbilities().flying)
+        if (CarpetSettings.creativeNoClip && entity instanceof Player player && player.isCreative() && player.getAbilities().flying)
             return PushReaction.IGNORE;
-        return entity.getPistonPushReaction();
+        return original.call(entity);
     }
 }
