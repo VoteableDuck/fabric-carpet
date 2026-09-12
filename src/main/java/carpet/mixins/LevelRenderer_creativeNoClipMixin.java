957f103ffb78ee45d3dfe8374c67cfff9f5c743b
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:35:40 +0800

    Compose creative no-clip renderer hook

--- src/main/java/carpet/mixins/LevelRenderer_creativeNoClipMixin.java
+++ src/main/java/carpet/mixins/LevelRenderer_creativeNoClipMixin.java
@@ -1,19 +1,19 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.LevelRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LevelRenderer.class)
 public class LevelRenderer_creativeNoClipMixin
 {
-    @Redirect(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isSpectator()Z"))
-    private boolean canSeeWorld(LocalPlayer clientPlayerEntity)
+    @WrapOperation(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isSpectator()Z"))
+    private boolean canSeeWorld(LocalPlayer player, Operation<Boolean> original)
     {
-        return clientPlayerEntity.isSpectator() || (CarpetSettings.creativeNoClip && clientPlayerEntity.isCreative());
+        return original.call(player) || (CarpetSettings.creativeNoClip && player.isCreative());
     }
-
 }
