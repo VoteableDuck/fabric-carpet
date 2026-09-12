70c26d7f43f957dad1610f887335fe687dede5f9
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 07:03:27 +0800

    Compose clean logs custom-name hook

--- src/main/java/carpet/mixins/LivingEntity_cleanLogsMixin.java
+++ src/main/java/carpet/mixins/LivingEntity_cleanLogsMixin.java
@@ -1,6 +1,8 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -9,7 +11,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gamerules.GameRules;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntity_cleanLogsMixin extends Entity
@@ -20,9 +21,12 @@ public abstract class LivingEntity_cleanLogsMixin extends Entity
         super(type, world);
     }
 
-    @Redirect(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hasCustomName()Z"))
-    private boolean shouldLogDeaths(LivingEntity livingEntity)
+    @WrapOperation(method = "die", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;hasCustomName()Z"))
+    private boolean shouldLogDeaths(LivingEntity livingEntity, Operation<Boolean> original)
     {
-        return livingEntity.hasCustomName() && livingEntity.level() instanceof ServerLevel serverLevel &&  CarpetSettings.cleanLogs && serverLevel.getGameRules().get(GameRules.SHOW_DEATH_MESSAGES);
+        return original.call(livingEntity)
+                && livingEntity.level() instanceof ServerLevel serverLevel
+                && CarpetSettings.cleanLogs
+                && serverLevel.getGameRules().get(GameRules.SHOW_DEATH_MESSAGES);
     }
 }
