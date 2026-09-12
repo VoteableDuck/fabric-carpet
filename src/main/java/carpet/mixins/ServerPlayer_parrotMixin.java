30122c0705ee4fd6fef6956731912b89003e498b
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 06:27:13 +0800

    Compose persistent parrot lifecycle hook

--- src/main/java/carpet/mixins/ServerPlayer_parrotMixin.java
+++ src/main/java/carpet/mixins/ServerPlayer_parrotMixin.java
@@ -1,6 +1,8 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
@@ -12,8 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
@@ -64,17 +64,17 @@ public abstract class ServerPlayer_parrotMixin extends Player {
     }
 
 
-    @Redirect(method = "handleShoulderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;removeEntitiesOnShoulder()V"))
-    private void onRespawnParrotsInstead(ServerPlayer serverPlayer)
+    @WrapOperation(method = "handleShoulderEntities", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;removeEntitiesOnShoulder()V"))
+    private void onRespawnParrotsInstead(ServerPlayer serverPlayer, Operation<Void> original)
     {
         if (!CarpetSettings.persistentParrots)
         {
-            this.removeEntitiesOnShoulder();
+            original.call(serverPlayer);
             return;
         }
         if (((getAbilities().invulnerable && this.fallDistance > 0.5F) || this.isInWater() || this.getAbilities().flying || isSleeping() || isInPowderSnow))
         {
-            this.removeEntitiesOnShoulder();
+            original.call(serverPlayer);
         }
     }
 }
