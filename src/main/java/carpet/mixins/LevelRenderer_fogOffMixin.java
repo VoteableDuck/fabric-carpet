bc78226f05da50c7586e29deffed3e9058379df9
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 07:02:25 +0800

    Compose fog attribute hook

--- src/main/java/carpet/mixins/LevelRenderer_fogOffMixin.java
+++ src/main/java/carpet/mixins/LevelRenderer_fogOffMixin.java
@@ -1,25 +1,32 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.GameRenderer;
-//import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.attribute.EnvironmentAttribute;
 import net.minecraft.world.attribute.EnvironmentAttributeProbe;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = GameRenderer.class, priority = 69420)
 public class LevelRenderer_fogOffMixin
 {
-    @Redirect(method = "renderLevel", require = 0, expect = 0, at = @At(
+    @WrapOperation(method = "renderLevel", require = 0, expect = 0, at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/attribute/EnvironmentAttributeProbe;getValue(Lnet/minecraft/world/attribute/EnvironmentAttribute;F)Ljava/lang/Object;"
     ))
-    private <Value> Value isReallyThick(EnvironmentAttributeProbe instance, EnvironmentAttribute<Value> environmentAttribute, float f)
+    private <Value> Value isReallyThick(
+            EnvironmentAttributeProbe instance,
+            EnvironmentAttribute<Value> environmentAttribute,
+            float f,
+            Operation<Value> original)
     {
-        if (CarpetSettings.fogOff) return (Value) Boolean.FALSE;
-        return instance.getValue(environmentAttribute, f);
+        if (CarpetSettings.fogOff) {
+            @SuppressWarnings("unchecked")
+            Value falseValue = (Value) Boolean.FALSE;
+            return falseValue;
+        }
+        return original.call(instance, environmentAttribute, f);
     }
-
 }
