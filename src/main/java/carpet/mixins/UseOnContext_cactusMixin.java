9b7c883e789bbd8e21904276017cf7f182860966
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 07:02:07 +0800

    Compose cactus facing hook

--- src/main/java/carpet/mixins/UseOnContext_cactusMixin.java
+++ src/main/java/carpet/mixins/UseOnContext_cactusMixin.java
@@ -1,23 +1,24 @@
 package carpet.mixins;
 
 import carpet.helpers.BlockRotator;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.UseOnContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(UseOnContext.class)
 public class UseOnContext_cactusMixin
 {
-    @Redirect(method = "getHorizontalDirection", at = @At(
+    @WrapOperation(method = "getHorizontalDirection", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/player/Player;getDirection()Lnet/minecraft/core/Direction;"
     ))
-    private Direction getPlayerFacing(Player playerEntity)
+    private Direction getPlayerFacing(Player playerEntity, Operation<Direction> original)
     {
-        Direction dir = playerEntity.getDirection();
+        Direction dir = original.call(playerEntity);
         if (BlockRotator.flippinEligibility(playerEntity))
         {
             dir = dir.getOpposite();
