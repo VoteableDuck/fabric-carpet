b2366c8bb7daeb8027dd9e7e74837a3164b2e3ca
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 02:20:05 +0800

    Use composable command stacktrace hook

--- src/main/java/carpet/mixins/Commands_customCommandsMixin.java
+++ src/main/java/carpet/mixins/Commands_customCommandsMixin.java
@@ -1,34 +1,20 @@
 package carpet.mixins;
 
-import carpet.CarpetServer;
 import carpet.CarpetSettings;
-import com.mojang.brigadier.CommandDispatcher;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.brigadier.ParseResults;
-import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import org.slf4j.Logger;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Commands.class)
 public abstract class Commands_customCommandsMixin
 {
-
-    @Shadow
-    @Final
-    private CommandDispatcher<CommandSourceStack> dispatcher;
-
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void onRegister(Commands.CommandSelection commandSelection, CommandBuildContext commandBuildContext, CallbackInfo ci) {
-        CarpetServer.registerCarpetCommands(this.dispatcher, commandSelection, commandBuildContext);
-    }
-
     @Inject(method = "performCommand", at = @At("HEAD"))
     private void onExecuteBegin(ParseResults<CommandSourceStack> parseResults, String string, CallbackInfo ci)
     {
@@ -42,17 +28,17 @@ public abstract class Commands_customCommandsMixin
         CarpetSettings.impendingFillSkipUpdates.set(false);
     }
 
-    @Redirect(method = "performCommand", at = @At(
+    @WrapOperation(method = "performCommand", at = @At(
                 value = "INVOKE",
                 target = "Lorg/slf4j/Logger;isDebugEnabled()Z",
                 remap = false
             ),
         require = 0
     )
-    private boolean doesOutputCommandStackTrace(Logger logger)
+    private boolean doesOutputCommandStackTrace(Logger logger, Operation<Boolean> original)
     {
         if (CarpetSettings.superSecretSetting)
             return true;
-        return logger.isDebugEnabled();
+        return original.call(logger);
     }
 }
