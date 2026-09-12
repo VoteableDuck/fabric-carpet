625c663d7871d29730ad761d5da90bd488fd07d3
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-12 22:56:23 +0800

    Compose level fill updates with NeoForge

--- src/main/java/carpet/mixins/Level_fillUpdatesMixin.java
+++ src/main/java/carpet/mixins/Level_fillUpdatesMixin.java
@@ -1,6 +1,8 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -8,7 +10,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Level.class)
 public abstract class Level_fillUpdatesMixin
@@ -21,13 +22,16 @@ public abstract class Level_fillUpdatesMixin
         return original;
     }
 
-    @Redirect(method = "setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;II)Z", at = @At( //setBlockState main
+    @WrapOperation(method = "setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;II)Z", at = @At( //setBlockState main
             value = "INVOKE",
             target  = "Lnet/minecraft/world/level/Level;updateNeighborsAt(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;)V"
     ))
-    private void updateNeighborsMaybe(Level world, BlockPos blockPos, Block block)
+    private void updateNeighborsMaybe(Level world, BlockPos blockPos, Block block, Operation<Void> original)
     {
-        if (!CarpetSettings.impendingFillSkipUpdates.get()) world.updateNeighborsAt(blockPos, block);
+        if (!CarpetSettings.impendingFillSkipUpdates.get())
+        {
+            original.call(world, blockPos, block);
+        }
     }
 
 }
