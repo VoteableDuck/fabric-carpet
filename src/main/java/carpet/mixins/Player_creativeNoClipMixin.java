4a3ad86a98b5383004a11e763c7b9739d158529b
VoteableDuck <123856601+VoteableDuck@users.noreply.github.com>
2026-09-13 07:02:15 +0800

    Compose creative no-clip player hooks

--- src/main/java/carpet/mixins/Player_creativeNoClipMixin.java
+++ src/main/java/carpet/mixins/Player_creativeNoClipMixin.java
@@ -1,13 +1,14 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Player.class)
 public abstract class Player_creativeNoClipMixin extends LivingEntity
@@ -17,31 +18,36 @@ public abstract class Player_creativeNoClipMixin extends LivingEntity
         super(type, world);
     }
 
-    @Redirect(method = "tick", at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/player/Player;isSpectator()Z")
-    )
-    private boolean canClipTroughWorld(Player playerEntity)
+    private boolean carpet$canNoClip(Player playerEntity, Operation<Boolean> original)
     {
-        return playerEntity.isSpectator() || (CarpetSettings.creativeNoClip && playerEntity.isCreative() && playerEntity.getAbilities().flying);
-
+        return original.call(playerEntity)
+                || (CarpetSettings.creativeNoClip && playerEntity.isCreative() && playerEntity.getAbilities().flying);
     }
 
-    @Redirect(method = "aiStep", at = @At(
+    @WrapOperation(method = "tick", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/player/Player;isSpectator()Z")
     )
-    private boolean collidesWithEntities(Player playerEntity)
+    private boolean canClipThroughWorld(Player playerEntity, Operation<Boolean> original)
     {
-        return playerEntity.isSpectator() || (CarpetSettings.creativeNoClip && playerEntity.isCreative() && playerEntity.getAbilities().flying);
+        return carpet$canNoClip(playerEntity, original);
     }
 
-    @Redirect(method = "updatePlayerPose", at = @At(
+    @WrapOperation(method = "aiStep", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/entity/player/Player;isSpectator()Z")
     )
-    private boolean spectatorsDontPose(Player playerEntity)
+    private boolean collidesWithEntities(Player playerEntity, Operation<Boolean> original)
     {
-        return playerEntity.isSpectator() || (CarpetSettings.creativeNoClip && playerEntity.isCreative() && playerEntity.getAbilities().flying);
+        return carpet$canNoClip(playerEntity, original);
+    }
+
+    @WrapOperation(method = "updatePlayerPose", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/entity/player/Player;isSpectator()Z")
+    )
+    private boolean spectatorsDontPose(Player playerEntity, Operation<Boolean> original)
+    {
+        return carpet$canNoClip(playerEntity, original);
     }
 }
